5cf0e07fc736a7216ce73f6c51ee2b26190fe2c0
ahmedoy <ahmedoy34@gmail.com>
2024-02-29 11:21:47 +0200

    Connected Agent to Game

--- game.py
+++ game.py
@@ -1,13 +1,19 @@
 import keyboard
 import screen
 import board
+import agents
 
 
 class Game:
     def __init__(self,  display=False):
-        initial_state = [1,2,0,3,4,5,6,7,8]
+        initial_state = [1,4,2,
+                         6,3,7,
+                         8,0,5]
         self.board = board.Board(initial_state)
         self.display = display
+        self.agent = agents.AStarAgent(self.board.get_state())
+        self.action_plan = self.agent.solve() #TODO handle if no solution found (empty action plan list)
+        self.action_idx = 0   
         self.key_responses()
         if display:
             self.create_window()
@@ -18,26 +24,29 @@ class Game:
         while not self.board.check_win():
             pass
         
-        #Add Game Response after winning here
+        #TODO Add Game Response after winning here
         
     def key_responses(self):
-        keyboard.on_press_key("up", self.on_arrow_key)
-        keyboard.on_press_key("down", self.on_arrow_key)
         keyboard.on_press_key("left", self.on_arrow_key)
         keyboard.on_press_key("right", self.on_arrow_key)
 
 
     def on_arrow_key(self, event):
-        controls_dict = {'up':'down', 'down':'up', 'right':'left', 'left':'right'} #inverted controls are easier for the player        
-        if self.board.take_action(controls_dict[event.name]) == True:
+        inverted_controls = {'up':'down', 'down':'up', 'right':'left', 'left':'right'} #inverted controls for replaying action plan backwards
+        if event.name == 'right' and self.action_idx < len(self.action_plan):
+            self.board.take_action(self.action_plan[self.action_idx])
+            self.action_idx +=1
             self.game_screen.display(self.board.get_state())
-
+        elif event.name == 'left' and self.action_idx > 0:
+            self.action_idx -= 1
+            self.board.take_action(inverted_controls[self.action_plan[self.action_idx]])
+            self.game_screen.display(self.board.get_state())
 
     
     def create_window(self):
-        tile_size = 60
-        window_height = 240
-        window_width = 240
+        tile_size = 80
+        window_height = 500
+        window_width = 1000
         self.game_screen = screen.Screen(tile_size, window_height, window_width)
         self.game_screen.display(self.board.get_state())
         self.game_screen.run()          
